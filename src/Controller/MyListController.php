3aacee29533ec9065ba4df3f941ef5f5a5b50da9
Ilies <iliesrimani.work@gmail.com>
2024-12-12 12:07:53 +0100

    feat: page subscription

--- src/Controller/MyListController.php
+++ src/Controller/MyListController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,8 +12,11 @@ class MyListController extends AbstractController
 	#[Route('/my_list', name: 'my_list')]
 	public function my_list(): Response
 	{
+		/**@var User $user */
+		$user = $this->getUser();
+
 		return $this->render('pages/my_list/index.html.twig', [
-			'controller_name' => 'MyListController',
+			'playlists' => $user->getPlaylists(),
 		]);
 	}
 }
--- src/Controller/SubscriptionController.php
+++ src/Controller/SubscriptionController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Subscription;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SubscriptionController extends AbstractController
 {
-	#[Route('/subscription', name: 'user_subscriptions')]
-	public function index(): Response
+	#[Route('/subscription', name: 'user_subscriptions_show')]
+	public function index(EntityManagerInterface $manager): Response
 	{
-		return $this->render('subscription/index.html.twig', [
-			'controller_name' => 'SubscriptionController',
+		/**@var User $user */
+		$user = $this->getUser();
+
+		return $this->render('pages/subscribe/index.html.twig', [
+			'currentSubscription' => $user->getCurrentSubscription(),
+			'subscriptions' => $manager->getRepository(Subscription::class)->findAll(),
 		]);
 	}
 }
